b5a1fb704d3a3923b187cade3b9f73a4d73612f7
halalan <halalanrizkon52@gmail.com>
2019-04-01 21:21:36 +0700

    Composing Components

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+
+export default class Counter extends Component {
+ 
+ state = {
+ count: 0
+ };
+
+handleIncrement = Product => {
+  console.log(Product);
+    this.setState({count: this.state.count + 1});
+};
+doHandleIncrement = () => {
+  this.handleIncrement({ id: 1});
+};
+
+    render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button 
+         onClick={() => this.doHandleIncrement(this.props.Product)}
+          className="btn btn-secondary btn-sm"
+          >
+           klik di sini
+           </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+      let classes = "badge m-2 badge-";
+      classes += this.state.count === 0? "warning" : "primary";
+      return classes;
+  }
+
+  formatCount() {
+    const {count} = this.state;
+    return count === 0 ? "masukan nilai mu" : count;
+  }
+}
