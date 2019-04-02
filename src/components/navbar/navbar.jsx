8b832ee4da3d7a088d9cbcf92cbfcdd2244ec71d
“halalan” <>
2019-04-02 13:16:35 +0700

    final Stateless Functional Components

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,18 @@
 
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
+const NavBar = (props) => {
+
     return (
       <nav className="navbar navbar-light bg-light">
-     
+        <a className="navbar-brand" href="#">
           Navbar{""}
           <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
+            {props.totalCounters}
           </span>
-        
+        </a>
       </nav>
     );
-  }
-}
+  };
 
 export default NavBar;
